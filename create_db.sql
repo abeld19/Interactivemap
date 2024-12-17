e85abc61b5221d31f582d49f8202f2182707380d
abeld19 <adani001@campus.goldsmiths.ac.uk>
2024-12-17 22:01:26 +0000

    db

--- create_db.sql
+++ create_db.sql
@@ -16,4 +16,18 @@ CREATE TABLE contacts (
   email VARCHAR(255) NOT NULL,
   message TEXT NOT NULL
 );
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
+-- Create a new user
+CREATE USER 'interactivemap'@'localhost' IDENTIFIED BY 'interactive1234';
+
+-- Grant all privileges on the 'map' database to the new user
+GRANT ALL PRIVILEGES ON map.* TO 'interactivemap'@'localhost';
--- insert_test_data.sql
+++ insert_test_data.sql
@@ -1,3 +1,5 @@
 USE map;
 
 INSERT INTO sightings (speciesName, date, description, image) VALUES ('Bald Eagle', '2018-01-01', 'A majestic bird of prey.', 'pond.png');
+
+INSERT INTO users (username, first_name, last_name, email, password) VALUES ('abel', 'abel', 'Daniel', 'abel@gmail.com', 'hashedpassword');
